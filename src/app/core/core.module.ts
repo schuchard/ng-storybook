fed0386f258effba5a41df0c4514f48ab9383c40
kevin schuchard <schuchard.kevin@gmail.com>
2019-03-30 07:11:14 -0600

    rename to core, add dashboard

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule,
+} from '@angular/material';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [NavbarComponent, DashboardComponent],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+  ],
+  exports: [NavbarComponent, DashboardComponent],
+})
+export class CoreModule {}
