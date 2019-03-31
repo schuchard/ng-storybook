89bfd038cb765d83572dc8c0787f0571bfb8a46e
kevin schuchard <schuchard.kevin@gmail.com>
2019-03-31 07:33:04 -0600

    refactor modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { RouterModule } from '@angular/router';
-import { MaterialModule } from './core/material.module';
-import { ReactiveFormsModule } from '@angular/forms';
 import { CoreComponentsModule } from './core/core.components.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    CoreModule,
     BrowserModule,
-    AppRoutingModule,
+    CoreModule,
     CoreComponentsModule,
-    RouterModule,
-    MaterialModule,
-    ReactiveFormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
