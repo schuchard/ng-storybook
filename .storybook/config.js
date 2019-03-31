fdf89cb32d81d4a087356120c68ae20e1f7711ad
kevin schuchard <schuchard.kevin@gmail.com>
2019-03-31 09:16:53 -0600

    WIP: navigation - add console to actions output

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,12 @@
 import { configure } from '@storybook/angular';
+import { setConsoleOptions } from '@storybook/addon-console';
 
 // automatically import all files ending in *.stories.ts
 const req = require.context('../src', true, /\.stories\.ts$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
-
+setConsoleOptions({
+  panelExclude: [],
+});
 configure(loadStories, module);
--- src/app/core/navbar/navbar.stories.ts
+++ src/app/core/navbar/navbar.stories.ts
@@ -1,12 +1,13 @@
 import { storiesOf } from '@storybook/angular';
 import { NavbarComponent } from './navbar.component';
 import { CoreModule } from '../core.module';
+import { RouterModule } from '@angular/router';
 
 storiesOf(`Core/navbar`, module).add(
   'Default',
   () => ({
     component: NavbarComponent,
-    moduleMetadata: {imports: [CoreModule]}
+    moduleMetadata: {imports: [CoreModule, RouterModule.forRoot([])]}
   }),
   { notes: 'My notes on a button with emojis' }
 );
