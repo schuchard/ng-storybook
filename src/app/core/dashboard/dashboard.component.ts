84107d098036fe2c4763b2e21995fbdd943b01e2
kevin schuchard <schuchard.kevin@gmail.com>
2019-04-01 21:24:55 -0600

    dashboard props

--- src/app/core/dashboard/dashboard.component.ts
+++ src/app/core/dashboard/dashboard.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { Component, Input } from '@angular/core';
+import { map, merge, combineLatest } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { BehaviorSubject } from 'rxjs';
+
+export interface ColType {
+  title: string;
+  cols: number;
+  rows: number;
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -8,24 +15,34 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
+  cardData = new BehaviorSubject<ColType[]>([
+    { title: 'Card 1', cols: 2, rows: 1 },
+    { title: 'Card 2', cols: 1, rows: 1 },
+    { title: 'Card 3', cols: 1, rows: 2 },
+    { title: 'Card 4', cols: 1, rows: 1 },
+  ]);
+
+  @Input()
+  set addCard(cards: ColType) {
+    this.cardData.next([cards, ...this.cardData.value.slice()]);
+  }
+
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 },
-        ];
+    combineLatest(this.cardData),
+    map(([bp, cards]) => {
+      if (bp.matches) {
+        // shrink for mobile
+        return cards.map(c => {
+          return {
+            ...c,
+            cols: 1,
+            rows: 1,
+          };
+        });
       }
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 },
-      ];
+      return cards;
     })
   );
 
--- src/app/core/dashboard/dashboard.stories.ts
+++ src/app/core/dashboard/dashboard.stories.ts
@@ -1,12 +1,17 @@
 import { storiesOf } from '@storybook/angular';
 import { CoreModule } from '../core.module';
 import { DashboardComponent } from './dashboard.component';
+import { action, configureActions } from '@storybook/addon-actions';
 
-storiesOf(`Core/dashboard`, module).add(
-  'Default',
-  () => ({
+storiesOf(`Core/dashboard`, module)
+  .add('Default', () => ({
     component: DashboardComponent,
-    moduleMetadata: {imports: [CoreModule]}
-  }),
-  { notes: 'My notes on a button with emojis' }
-);
+    moduleMetadata: { imports: [CoreModule] },
+  }))
+  .add('Add Card', () => ({
+    component: DashboardComponent,
+    props: {
+      addCard: { title: 'Card 5', cols: 2, rows: 1 }
+    },
+    moduleMetadata: { imports: [CoreModule] },
+  }));
