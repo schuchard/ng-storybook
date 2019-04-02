8e593ec216642e49dc8fac1e614032016e37fe04
kevin schuchard <schuchard.kevin@gmail.com>
2019-04-01 21:40:17 -0600

    add knob example dashboard

--- src/app/core/dashboard/dashboard.stories.ts
+++ src/app/core/dashboard/dashboard.stories.ts
@@ -2,8 +2,22 @@ import { storiesOf } from '@storybook/angular';
 import { CoreModule } from '../core.module';
 import { DashboardComponent } from './dashboard.component';
 import { action, configureActions } from '@storybook/addon-actions';
+import { select, withKnobs } from '@storybook/addon-knobs';
 
-storiesOf(`Core/dashboard`, module)
+const label = 'Cards';
+const options = {
+  '2X1 Narrow': { title: '2X1 Narrow', cols: 2, rows: 1 },
+  '1X1 Square': { title: '1X1 Square', cols: 1, rows: 1 },
+  '2X2 Wide': { title: '2X2 Wide', cols: 2, rows: 2 },
+  '1X2 Tall': { title: '1X2 Tall', cols: 1, rows: 2 },
+};
+const defaultValue = { title: '2X1 Narrow', cols: 2, rows: 1 };
+const groupId = 'GROUP-ID1';
+
+const stories = storiesOf(`Core/dashboard`, module);
+
+stories.addDecorator(withKnobs);
+stories
   .add('Default', () => ({
     component: DashboardComponent,
     moduleMetadata: { imports: [CoreModule] },
@@ -11,7 +25,7 @@ storiesOf(`Core/dashboard`, module)
   .add('Add Card', () => ({
     component: DashboardComponent,
     props: {
-      addCard: { title: 'Card 5', cols: 2, rows: 1 }
+      addCard: select(label, options, defaultValue, groupId),
     },
     moduleMetadata: { imports: [CoreModule] },
   }));
